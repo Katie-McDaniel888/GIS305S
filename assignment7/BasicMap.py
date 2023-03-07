283e7dffd35979667da0b89f2f83702a107ae954
Katie-McDaniel888 <125925366+Katie-McDaniel888@users.noreply.github.com>
2023-03-07 15:45:46 -0700

    Added get.Bounds and print statement

--- assignment7/BasicMap.py
+++ assignment7/BasicMap.py
@@ -55,6 +55,11 @@ if __name__ == "__main__":
     my_map = BasicMap("-105.2705", "40.015", "0.5", "0.25")
     my_map.describe()
 
+try:
+    print("CAlculating bounds...")
+        my_map.get_bounds()
+    except TypeError:
+        print("Error: in get_Bounds - Check your input values, they must be numbers!")
 
     print('Calculating bounds...')
     my_map.get_bounds()
