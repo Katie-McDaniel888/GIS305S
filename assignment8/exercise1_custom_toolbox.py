d8d85130fadd00782713bc5116f9f9b872d1a021
Katie-McDaniel888 <125925366+Katie-McDaniel888@users.noreply.github.com>
2023-04-05 13:18:54 -0600

    Assignment11

--- assignment8/exercise1_custom_toolbox.py
+++ assignment8/exercise1_custom_toolbox.py
@@ -1,13 +1,7 @@
 import arcpy
-
-
 def intersect(layer_list, input_lyr_name):
-
     # Run a intersect analysis between the two buffer layers (needs to be a list of layers to intersect)
     arcpy.Intersect_analysis(layer_list, input_lyr_name)
-
-
-
 def buffer_layer(input_gdb, input_layer, dist):
     # Run a buffer analysis on the input_layer with a user specified distance
 
@@ -31,25 +25,18 @@ def main():
     # Buffer cities
     input_gdb = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Data\Admin\AdminData.gdb\USA\\"
 
-    # Change me this next line below to use GetParamters!!
-    dist = input("What buffer distance do you want to use?")
+    dist = arcpy.GetParameterAsText(0)
 
     buf_cities = buffer_layer(input_gdb, "cities", dist)
 
     # Change me this next line below to use GetParamters!!
     print("Buffer layer " + buf_cities + " created.")
 
-    # Buffer rivers
-    # Change me this next line below to use GetParamters!!
-    dist = input("What buffer distance do you want to use?")
+    dist = arcpy.GetParameterAsText(0)
     buf_rivers = buffer_layer(input_gdb, "us_rivers", dist)
     print("Buffer layer " + buf_rivers + " created.")
 
-    # Define lyr_list variable
-    # with names of input layers to intersect
-    # Ask the user to define an output layer name
-    # Change me this next line below to use GetParamters!!
-    intersect_lyr_name = input("What is the name for your output layer resulting from the intersect analysis? ")
+    intersect_lyr_name = arcpy.GetParameterAsText(0)
     lyr_list = [buf_rivers, buf_cities]
     intersect(lyr_list, intersect_lyr_name)
     print(f"New intersect layer generated called: {intersect_lyr_name}")
