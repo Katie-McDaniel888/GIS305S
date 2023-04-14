d66aa3ca60a68e47544fae0e5f8d17bcd883717b
Katie-McDaniel888 <125925366+Katie-McDaniel888@users.noreply.github.com>
2023-04-13 19:46:46 -0600

    Lab_2 by Katie McDaniel

--- Lab2_McDaniel/Lab_2.py
+++ Lab2_McDaniel/Lab_2.py
@@ -1,16 +1,8 @@
 import arcpy
 import requests
 import yaml
+import csv
 
-
-def setup():
-    arcpy.env.workspace = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb"
-    with open('config/wnvoutbreak.yaml') as f:config_dict = yaml.load(f, loader=yml.FullLoader)
-    return config_dict
-def etl():
-    print("Take the survey")
-    etl_instance=GSheetsEtl("https://foo_bar.com", "C:/Users/my.gdb", "C:/Users", "GSheets")
-    etl_instance.process()
 class SpatialEtl:
     def __init__(self,config_dict):
         self.config_dict = config_dict
@@ -24,37 +16,56 @@ class SpatialEtl:
 class GSheetsEtl(SpatialEtl):
     config_dict = None
     def __init__(self,config_dict):
-        super().__init__(self.config_dict)
+        super().__init__(config_dict)
+
     def extract(self):
-        print(f"Extracting addresses from {self.config_dict.get('remote_url')}" f"to {self.config_dict.get('proj_dir')}")
-        #file = urllib.request.urlopen("https://docs.google.com/forms/d/e/1FAIpQLSe1QzJGuFFAb0xQi2VxWE9bmc85Oxv4a3SNnuAvgVgaZgVySg/viewform?usp=sf_link)
-        r = requests.get(self.config_dict.get('remote_url'))
+        print(f"Extracting addresses from {self.config_dict}")
+        # file = urllib.request.urlopen("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaJ_1xRhGQAOSITkgn_C1wfPSnPX0BA37XuftlXVfVrpjfj4J3BHPu1soGeUtNt3XjLI1G_HT2Fy69/pub?output=csv")
+
+        r = requests.get(self.config_dict.get("remote_url"))
+        r.encoding = "utf-8"
+        data = r.text
+        with open(r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab 2\addresses.csv", "w") as output_file:
+            output_file.write(data)
     def load(self):
         #set environment
-        arcpy.env.workspace = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb"
-        arcpy.env.orderwriteOutput = True
-        in_table = r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\new_addresses.csv"
+        arcpy.env.workspace = r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab1\Katie_McDaniel_Lab1\WestNileOutbreak.gdb"
+        arcpy.env.overwriteOutput = True
+        in_table = r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab 2\new_addresses.csv"
         out_feature_class = "avoid_points"
         x_coords = "X"
         y_coords = "Y"
         arcpy.management.XYTableToPoint(in_table, out_feature_class, x_coords, y_coords)
         print(arcpy.GetCount_management(out_feature_class))
+
+    def transform(self):
+        transformed_file = open(r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab 2\new_addresses.csv", "w")
+        transformed_file.write("X,Y,Type\n")
+        with open(r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab 2\addresses.csv", "r") as partial_file:
+            csv_dict = csv.DictReader(partial_file, delimiter=',')
+            for row in csv_dict:
+                address = row["Street Address"] + " Boulder CO"
+                print(address)
+                geocode_url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=" + address + "&benchmark=2020&format=json"
+                print(geocode_url)
+                r = requests.get(geocode_url)
+
+                resp_dict = r.json()
+                x = resp_dict['result']['addressMatches'][0]['coordinates']['x']
+                y = resp_dict['result']['addressMatches'][0]['coordinates']['y']
+                transformed_file.write(f"{x},{y},Residential\n")
+
+        transformed_file.close()
     def process(self):
         self.extract()
         self.transform()
         self.load()
 
-
-#define global
-#input_path = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb{layer_name}"
-
-mosquito=input_path.format(layer_name=r"\Mosquito_Larval_Sites")
-osmp=input_path.format(layer_name=r"\OSMP_Properties")
-lakes_reservoirs=input_path.format(layer_name=r"\Lakes_and_Reservoirs___Boulder_County")
-wetlands=input_path.format(layer_name=r"\Wetlands")
-avoid_points=input_path.format(layer_name=r"\avoid_points")
-
-list_lyrs = ["buff_lakes_reservoirs", "buff_mosquitos", "buff_wetlands", "buff_osmp", "buff_avoid_points"]
+def setup():
+    with open('config/wnvoutbreak.yaml') as f:
+        config_dict = yaml.load(f, Loader=yaml.FullLoader)
+    arcpy.env.workspace = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb"
+    return config_dict
 
 def buffer(layer_name, bufDist):
     # Buffer the incoming layer by the buffer distance
@@ -64,6 +75,7 @@ def buffer(layer_name, bufDist):
     arcpy.Buffer_analysis(osmp, "buff_osmp", "1500 Feet")
     arcpy.Buffer_analysis(lakes_reservoirs, "buff_lakes_reservoirs", "1500 Feet")
     arcpy.Buffer_analysis(wetlands, "buff_wetlands", "1500 Feet")
+    arcpy.Buffer_analysis(avoid_points, "buff_avoid_points", "1500 Feet")
 
     print(f"Buffering{layer_name} to generate {out_feature_class}")
 
@@ -78,10 +90,20 @@ def intersect(list_lyrs):
 
 def main():
     # define default workspace
-    arcpy.env.workspace = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb"
+    arcpy.env.workspace = r"C:\Users\ka003737\Downloads\Spring_2023\GIS3005\Lab1\Katie_McDaniel_Lab1\WestNileOutbreak.gdb"
     list_lyrs = ["buff_lakes_reservoirs", "buff_mosquitos", "buff_wetlands", "buff_osmp"]
     output_lyr_name= intersect(list_lyrs)
+    input_path = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb{layer_name}"
 
+    mosquito = input_path.format(layer_name=r"\Mosquito_Larval_Sites")
+    osmp = input_path.format(layer_name=r"\OSMP_Properties")
+    lakes_reservoirs = input_path.format(layer_name=r"\Lakes_and_Reservoirs___Boulder_County")
+    wetlands = input_path.format(layer_name=r"\Wetlands")
+    avoid_points = input_path.format(layer_name=r"\avoid_points")
+
+    list_lyrs = ["buff_lakes_reservoirs", "buff_mosquitos", "buff_wetlands", "buff_osmp", "buff_avoid_points"]
+    eraseOutput = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb\buff_avoid_points"
+    arcpy.analysis.Erase(output_lyr_name, eraseOutput)
 def spatial_join():
     # Spatial Join
     join_features = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb\buff_lakes_reservo_Intersect1"
@@ -90,14 +112,11 @@ def spatial_join():
     arcpy.analysis.SpatialJoin(target_features, join_features, out_feature_class)
 
     print(f"My spatial join method")
-def erase():
-    eraseOutput = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb\buff_avoid_points"
-    arcpy.analysis.Erase(buff_avoid_points, eraseOutput)
 
 if __name__ == '__main__':
     global config_dict
     config_dict = setup()
     print(config_dict)
-    etl()
+    gsheetsetl = GSheetsEtl(config_dict)
+    gsheetsetl.process()
     main()
-
